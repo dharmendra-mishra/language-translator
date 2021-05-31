380de9e92cc69486f9ff0c56db63699d03bb86d5
Dharmendra <dharmish@cisco.com>
2021-05-31 11:40:35 +0530

    Add support for creation of python virtualenv for Windows machine

--- start.sh
+++ start.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 VENV_DIR=./venv_python3
+echo "Removing any existing python virtual environment with the name: " $VENV_DIR
 rm -rf $VENV_DIR
-python3 -m venv $VENV_DIR
 
-chmod 775 $VENV_DIR/bin/activate
-source $VENV_DIR/bin/activate
-pip3 install -r requirements.txt
+echo "Going to create python virtual environment"
+# For MAC/Linux machine
+#python -m venv $VENV_DIR
+#chmod 775 $VENV_DIR/bin/activate
+#source $VENV_DIR/bin/activate
 
-python3 api.py
+# For windows machine
+python -m venv $VENV_DIR
+chmod 775 $VENV_DIR/Scripts/activate
+source $VENV_DIR/Scripts/activate
+
+echo "Python virtual environment successfully created!!!"
+echo "Python version: "
+
+python --version
+echo "Pip version: "
+pip --version
+
+echo "Install dependencies..."
+pip install -r requirements.txt
+
+python api.py
